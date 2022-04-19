1260f51aad2cce76c76e4c37a9dd33af24625585
Tuanndq <quangtuanxyz@gmail.com>
2022-04-19 12:12:56 +0700

    init

---
+++ Donation.5.0/app/src/main/java/ie/app/main/DonationApp.java
@@ -0,0 +1,45 @@
+package ie.app.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+//import ie.app.database.DBManager;
+import ie.app.models.Donation;
+
+public class DonationApp extends Application
+{
+    public final int       target       = 10000;
+    public int             totalDonated = 0;
+    public List <Donation> donations    = new ArrayList<Donation>();
+
+
+    public boolean newDonation(Donation donation)
+    {
+        boolean targetAchieved = totalDonated > target;
+        if (!targetAchieved)
+        {
+            donations.add(donation);
+            totalDonated += donation.amount;
+        }
+        else
+        {
+            Toast.makeText(this, "Target Exceeded!", Toast.LENGTH_SHORT).show();
+        }
+        return targetAchieved;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+    }
+
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Bài tập Phát triển ứng dụng di động - UET thầy Tân
+
+Tiến độ: 
+
+Donation 5.0 chưa hoàn chỉnh
+
+bài 14, 15 chưa hoàn thiện
+
+Repo mình clone ở  https://github.com/vietcuongk99/mobileHomework
